e6c835db44370e742322570cee67c526846ebd55
Lawrence <lcchou2@gmail.com>
2019-01-21 12:14:43 -0800

    Accept name and message and completed assignment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,58 @@
-
-import post from './post.jsx'
-
+import Post from './post.jsx'
 import React from 'react';
 
 
 class App extends React.Component {
+
   constructor(props) {
-    super(props);
+    super(props)
+
+    this.state = {
+      currName: 'anon',
+      currMessage: 'yes',
+      serverMess: 'please wrk'
+    }
+
   }
 
+  nameInput(event){
+    this.setState({
+      currName: event.target.value
+    })
+  }
+
+  messageInput(event){
+    this.setState({
+      currMessage: event.target.value
+    })
+  }
+
+  submitHandler(event){
+    event.preventDefault();
+
+    var newMessage = {
+      name: this.state.currName,
+      message: this.state.currMessage
+    }
+
+    Post.create(newMessage, (data) => {
+      this.setState({
+        serverMess: data
+      })
+    })
+  }
+  
   render() {
     return (
       <div>
         <div className='first'>
           <h1>Server Response:</h1>
+          <h2>{this.state.serverMess}</h2>
         </div>
         <form>
-          Name: <input type="text" name="name" />
-          Message: <input type="text" name="message" />
-          <input type='submit' value='SendMessage' />
+          Name: <input type="text" onChange= {this.nameInput.bind(this)}/>
+          Message: <input type="text" onChange={this.messageInput.bind(this)}/>
+          <button onClick={this.submitHandler.bind(this)}>SubmitMessage</button>
         </form>
 
       </div>
@@ -26,6 +60,8 @@ class App extends React.Component {
     )
   }
 
+  
+
 }
 
-export default App
+export default App;
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,17 +1,25 @@
-var post = () => {
-  $.ajax({
-    url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting',
-    dataType: 'json',
-    type: 'POST',
-    contentType: 'application/json',
-    // data: JSON.stringify({ "first-name": $('#first-name').val(), "last-name": $('#last-name').val() }),
-    // success: function (data, textStatus, jQxhr) {
-    //   $('#response pre').html(JSON.stringify(data));
-    // },
-    // error: function (jqXhr, textStatus, errorThrown) {
-    //   console.log(errorThrown);
-    // }
-  });
+
+var Post =  {
+
+
+  
+
+  create: function(message, successCB, errorCB = null){
+  
+    $.ajax({
+      url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf110/greeting',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error){
+        console.error('Bye', error)
+      }
+    })
+  }
+ 
+
 
 }
 
+export default Post
